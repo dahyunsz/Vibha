ab6e764178110d9ed9600efd826e265589ed7406
jamemini <msaechoa@gmail.com>
2021-03-21 04:54:27 +0700

    fix css

--- components/VibhaVideo.vue
+++ components/VibhaVideo.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="vibhavideo-container">
+    <img class="vibhavideo-bg" src="~static/vibha-video-bg.jpg">
     <div class="vibhavideo-center">
       <!-- <embed class="vibha-video" src="https://www.youtube.com/embed/FrOLeoXrvLU" type=""> -->
       <video class="vibha-video" controls>
@@ -19,14 +20,19 @@ export default class VibhaVideo extends Vue {}
 <style lang="scss" scoped>
 .vibhavideo-container{
     width: 100%;
-    background-image: url(~static/vibha-video-bg.jpg);
-    height: 950px   ;
+    height: 900px;
+}
+.vibhavideo-bg{
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  z-index: -1;
 }
 .vibhavideo-center{
     text-align: center;
     height: 100%;
-    padding-top: 6%;
-    padding-bottom: 4%;
+    padding-top: 2%;
+    padding-bottom: 2%;
 }
 .vibha-video{
     width: 60%;
@@ -35,11 +41,10 @@ export default class VibhaVideo extends Vue {}
 @media screen and (max-width: 768px) {
 .vibha-video{
     width: 80%;
-    height: 75%;
+    height: 80%;
 }
 .vibhavideo-container{
     width: 100%;
-    background-image: url(~static/vibha-video-bg.jpg);
     height: 300px   ;
 }
 .vibhavideo-center{
